4830f310b92ecdd61653dcf15718e278ca29d727
zzz <tenkanse@hotmail.com>
2017-12-30 16:30:46 +0800

    增加拉api数据计算shell脚本

--- import.sh
+++ import.sh
@@ -2,5 +2,13 @@
 
 rm -rf /tmp/sde;
 unzip $1/*.zip -d /tmp;
+find /tmp/sde -name "*.DS_Store" -type f -delete;
+find /tmp/sde -name "*.DS_Store" -type d -delete;
 
-node import_sde_mongo.js clear bsd && node import_sde_mongo.js clear fsd && node import_sde_mongo.js transform bsd && node import_sde_mongo.js transform fsd && node import_sde_mongo.js transform universe && node import_sde_mongo.js import bsd && node import_sde_mongo.js import fsd;
+node import_sde_mongo.js clear bsd &&
+    node import_sde_mongo.js clear fsd &&
+    node import_sde_mongo.js transform bsd &&
+    node import_sde_mongo.js transform fsd &&
+    node import_sde_mongo.js transform universe &&
+    node import_sde_mongo.js import bsd &&
+    node import_sde_mongo.js import fsd;
