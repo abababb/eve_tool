6b704981832d919bc2aa32db0f00e4ab1290728b
zzz <tenkanse@hotmail.com>
2017-12-30 06:05:04 +0000

    脚本问题修改

--- import.sh
+++ import.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-rm -rf /tmp/sde
-unzip $1/sde.*.zip;
-mv $1/sde /tmp;
+rm -rf /tmp/sde;
+unzip $1/*.zip -d /tmp;
 
 node import_sde_mongo.js clear bsd && node import_sde_mongo.js clear fsd && node import_sde_mongo.js transform bsd && node import_sde_mongo.js transform fsd && node import_sde_mongo.js transform universe && node import_sde_mongo.js import bsd && node import_sde_mongo.js import fsd;
